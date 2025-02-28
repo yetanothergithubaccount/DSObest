966c5e2636829ddc7336d162ea08210dcb02afcd
yetanothergithubaccount <solveigh@googlemail.com>
2025-02-28 12:14:16 +0100

    print month in pdf subtitle

--- DSO_observation_planning.py
+++ DSO_observation_planning.py
@@ -150,7 +150,7 @@ my_DSO_list = ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11
 
 # Messier catalogue
 #my_DSO_list = ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11", "M12", "M13", "M14", "M15", "M16", "M17", "M18", "M19", "M20", "M21", "M22", "M23", "M24", "M25", "M26", "M27", "M28", "M29", "M30", "M31", "M32", "M33", "M34", "M35", "M36", "M37", "M38", "M39", "M40", "M41", "M42", "M43", "M44", "M45", "M46", "M47", "M48", "M49", "M50", "M51", "M52", "M53", "M54", "M55", "M56", "M57", "M58", "M59", "M60", "M61", "M62", "M63", "M64", "M65", "M66", "M67", "M68", "M69", "M70", "M71", "M72", "M73", "M74", "M75", "M76", "M77", "M78", "M79", "M80", "M81", "M82", "M83", "M84", "M85", "M86", "M87", "M88", "M89", "M90", "M91", "M92", "M93", "M94", "M95", "M96", "M97", "M98", "M99", "M100", "M101", "M102", "M103", "M104", "M105", "M106", "M107", "M108", "M109", "M110"]
-my_DSO_list = ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"]
+#my_DSO_list = ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10"]
 
 class DSO:
 
@@ -901,8 +901,8 @@ if __name__ == '__main__':
         print("Create PDF " + str(fileName) + "...")
         print("")
         print(pdfdata)
-      documentTitle ="Messier Objects Visibility in " + str(options.location)
-      title = "Messier Objects Visibility"
+      documentTitle ="DSO Visibility in " + str(options.location)
+      title = "DSO Visibility"
       subTitle = today.strftime("%d.%m.") + "-" + tomorrow.strftime("%d.%m.%Y") + " in " + str(options.location) + " (" + str(options.latitude) + ", " + str(options.longitude) + ")"  #"03.-04.03.2025 in Maspalomas (27.749997, -15.5666644)"
 
       elements = []
